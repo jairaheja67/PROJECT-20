959dee02bb9a027d7e90ea70625dba249420111a
jairaheja67 <73791673+jairaheja67@users.noreply.github.com>
2020-12-26 12:28:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var speed;
 var car1,car2,car3;
 var wall;
-var deformation
+var weight
 
 
 function setup() {
@@ -9,9 +9,9 @@ function setup() {
   speed=random(55,90)
   weight=random(400,1500)
   
-  car1=createSprite(50,200,20,20);
-  car2=createSprite(150,200,20,20)
-  car3=createSprite(250,200,20,20)
+  car1=createSprite(50,200,50,50);
+  car2=createSprite(150,200,50,50)
+  car3=createSprite(250,200,50,50)
 
   car1.velocityX=speed;
   car2.velocityX=speed;
@@ -21,13 +21,15 @@ function setup() {
    car2.shapeColor="pink"
    car3.shapeColor="grey"
    
-  wall=createSprite(1500,200,60,height/2)
+  wall1=createSprite(1500,200,60,height/2)
+  wall2=createSprite(1500,800,60,height/2)
+  wall3=createSprite(1500,1200,60,height/2)
 }
 
 function draw() {
   background(255,255,255); 
  
-  if(wall.x-car1.x<(car1.width+wall.width)/2){
+  if(wall1.x-car1.x<(car1.width+wall1.width)/2){
      
      
      var deformation=0.5*weight*speed*speed/22509
@@ -46,8 +48,8 @@ function draw() {
 
     }
      
-     if(wall.x-car2.x<(car2.width+wall.width)/2){
-      car1.velocity=0
+     if(wall2.x-car2.x<(car2.width+wall2.width)/2){
+      car2.velocity=0
       
       var deformation=0.5*weight*speed*speed/22509
       if(deformation>180)
@@ -62,9 +64,10 @@ function draw() {
       {
         car2.shapeColor="yellow"
       }
+    }
 
-      if(wall.x-car3.x<(car3.width+wall.width)/2){
-        car1.velocity=0
+      if(wall3.x-car3.x<(car3.width+wall3.width)/2){
+        car3.velocity=0
         
         var deformation=0.5*weight*speed*speed/22509
         if(deformation>180)
@@ -88,4 +91,4 @@ function draw() {
   }
    
   drawSprites();
-}}
+}
